9db30342e11429f58a4b4cdab5e57bf535561571
farhan-ibrahim <farhan5543@gmail.com>
2020-08-14 15:39:03 +0800

    add router-dom

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import "./App.css";
 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
-
-import Login from "./containers/auth/Login";
-
 import { store, persistor } from "./store/index";
+
+import Home from "./containers/home";
+// import Login from "./containers/auth/Login";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Login />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
       </PersistGate>
     </Provider>
   );
