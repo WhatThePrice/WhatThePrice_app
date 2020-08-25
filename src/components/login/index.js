c1ab5e8c2098034ec8a7508e51ab771108fa52f1
farhan-ibrahim <farhan5543@gmail.com>
2020-08-25 10:44:40 +0800

    add modal to login

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Actions from "actions";
 
+// Style
 import "./login.css";
+
+// Components
+import Modal from "components/modal";
+
 
 class Login extends React.Component{
     constructor(props){
@@ -13,6 +18,12 @@ class Login extends React.Component{
             email: "",
             name: "",
             isShowing: this.props.showBox,
+
+            // Auth status
+            showModal: false,
+            isLoading: true,
+            authStatus:"" ,
+            description:""
         }
     }
 
@@ -29,9 +40,13 @@ class Login extends React.Component{
         if(prevProps.getLoginData.isLoading && !getLoginData.isLoading) {
             console.log("Login Data", getLoginData);
             if(getLoginData && getLoginData.data.status === "success"){
-                alert("Success");
+                this.setState({
+                    isLoading:!this.state.isLoading,
+                    authStatus: "success",
+                    description: "You will redirect to homepage"
+                })
+                //alert("Success");
                 console.log("login success");
-                window.location = "/";
             } else if(getLoginData.error) {
                 alert("Failed", "Login failed");
             }
@@ -44,46 +59,56 @@ class Login extends React.Component{
             password: this.state.password,
         }
         this.props.onLogin(data);
+        this.setState({showModal:!this.state.showBox})
     }
 
     render() {
         return(
-            <div className={`loginContainer ${this.state.isShowing ? "active" : "hideBox"}`}>
-                <div className="loginHeader">
-                    <p>Start tracking your favourite item</p>
-                    <p onClick={() => this.setState({isShowing:!this.state.isShowing})}><i className="fa fa-times"></i></p>
-                </div>
-                <h1 className="loginTitle">Sign In</h1>
-                <div className="loginInputHolder">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        className="authInput"
-                        name="email" 
-                        placeholder="email" 
-                        onChange={(email) => this.setState({email:email.target.value})}
-                    />
-
-                    <label htmlFor="password">Password</label>
-                    <input
-                        className="authInput"
-                        type="password" 
-                        name="password" 
-                        placeholder="password" 
-                        onChange={(password) => this.setState({password:password.target.value})}
-                    />
-                    
-                    <div className="rememberPwdHolder">
-                        <input type="checkbox" />
-                        <p className="subtitleText">remember password</p>
+            <div>
+                <div className={`loginContainer ${this.state.isShowing ? "active" : "hideBox"}`}>
+                    <div className="loginHeader">
+                        <p>Start tracking your favourite item</p>
+                        <p onClick={() => this.setState({isShowing:!this.state.isShowing})}><i className="fa fa-times"></i></p>
                     </div>
+                    <h1 className="loginTitle">Sign In</h1>
+                    <div className="loginInputHolder">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            className="authInput"
+                            name="email" 
+                            placeholder="email" 
+                            onChange={(email) => this.setState({email:email.target.value})}
+                        />
 
-                    <button className="authButton" onClick={() => this.onLoginPressed()}>SIGN IN</button>
-                    <Link to="/"><p className="subtitleText">forgot username or password?</p></Link>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            className="authInput"
+                            type="password" 
+                            name="password" 
+                            placeholder="password" 
+                            onChange={(password) => this.setState({password:password.target.value})}
+                        />
+                        
+                        <div className="rememberPwdHolder">
+                            <input type="checkbox" />
+                            <p className="subtitleText">remember password</p>
+                        </div>
+
+                        <button className="authButton" onClick={() => this.onLoginPressed()}>SIGN IN</button>
+                        <Link to="/"><p className="subtitleText">forgot username or password?</p></Link>
+                    </div>
+                    <div className="loginFooter">
+                        <p className="subtitleText">Don't have account?</p> 
+                        <p className="subtitleText">Sign up <Link to="/register" onClick={this.props.onHideBox}>here</Link> to start tracking your price</p>
+                    </div>
                 </div>
-                <div className="loginFooter">
-                    <p className="subtitleText">Don't have account?</p> 
-                    <p className="subtitleText">Sign up <Link to="/register" onClick={this.props.onHideBox}>here</Link> to start tracking your price</p>
-                </div>
+                {this.state.showModal && <Modal 
+                    isLoading={this.state.isLoading}
+                    modalTitle="Auth"
+                    status={this.state.authStatus}
+                    description={this.state.description}
+                    onClick={() => window.location = "/"}
+                />}
             </div>
         )
     }
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -6,13 +6,6 @@ class Home extends React.Component{
         return(
             <div>
                 <p>this is home</p>
-                <Modal 
-                    isLoading="true"
-                    modalTitle="Login"
-                    status="success"
-                    description="You will redirect to homepage"
-                    onClick={() => console.log("modal button")}
-                />
             </div>
         )
     }
